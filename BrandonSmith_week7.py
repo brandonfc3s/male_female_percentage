7543d35b57f5024d822aa8f6954d7ea893228ad8
brandonfc3s <41172275+brandonfc3s@users.noreply.github.com>
2021-11-17 09:56:51 -0500

    Add files via upload

---
+++ BrandonSmith_week7.py
@@ -0,0 +1,6 @@
+male = float(input("How many male students?"))
+female = float(input("How many famale students?"))
+percentage_male = male/(male + female)*100
+percentage_female = female/(male +female)*100%
+print ("Percentage male", percentage_male)
+print ("Percentage female", percentage_female)
